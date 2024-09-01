0f3005c7466500fbff14dc9b4748bd387af39996
Furia <83609040+FuriaPaladins@users.noreply.github.com>
2024-09-01 13:04:17 +0100

    Asset update
    Most assets required for enka support

--- paladins/fetch_images.py
+++ paladins/fetch_images.py
@@ -2,7 +2,6 @@ import json
 import logging
 import os
 import random
-import asyncio
 
 import arc
 import arez
--- paladins/talents/clear.py
+++
@@ -1,8 +0,0 @@
-import os
-
-## Search in this directory and all subdirectories for files with "'", and replace them with ""
-for root, dirs, files in os.walk("."):
-    for file in files:
-        if "'" in file:
-            os.rename(os.path.join(root, file), os.path.join(root, file.replace("'", '')))
-            print(f"""Renaming {file} to {file.replace("'", '')}""")
